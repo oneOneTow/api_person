6c2deee7579f691bf799eb8fe0b7cbe57aa9a8ee
luzhiqing <luzhiqing@ccbtrust.com.cn>
2019-02-25 20:32:52 +0800

    修改下划线

--- src/main/java/com/ccbcfx/learn/ApiApplication.java
+++ src/main/java/com/ccbcfx/learn/ApiApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
-public class Application {
+public class ApiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
+        SpringApplication.run(ServiceApplication.class,args);
     }
 }
